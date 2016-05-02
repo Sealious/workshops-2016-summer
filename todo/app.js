3a30df3ebb729814c017ffd46d7a66084eba0364
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-05-02 23:42:58 +0200

    Add app.js

---
+++ todo/app.js
@@ -0,0 +1,56 @@
+var tasks = [
+  {
+    content: 'Odrobić zadanie domowe',
+    completed: false
+  },
+  {
+    content: 'Napisać maila',
+    completed: false
+  },
+  {
+    content: 'Spakować się',
+    completed: false
+  },
+  {
+    content: 'Kupić klapki',
+    completed: false
+  },
+  {
+    content: 'Zjeść obiad',
+    completed: true
+  }
+];
+
+function addTask() {
+  var input = document.getElementById('todo-content');
+  var task = {content: input.value, completed: false};
+  input.value = '';
+  tasks.push(task);
+  showTask(task);
+}
+
+function showTask(task) {
+  var list = document.querySelector('ul');
+  var newTask = document.createElement('li');
+  var taskLabel = document.createElement('span');
+
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  if(task.completed) {
+    checkbox.checked = 'checked';
+  }
+  newTask.appendChild(checkbox);
+
+  taskLabel.textContent = task.content;
+  newTask.appendChild(taskLabel);
+
+  list.appendChild(newTask);
+}
+
+function showTasks() {
+  tasks.forEach(showTask);
+}
+
+showTasks();
+
+document.getElementById('todo-form').onsubmit = addTask;
