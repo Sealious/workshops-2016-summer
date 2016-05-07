bd272b8227fe13a0475bbc511798b265abeebb3e
matiduck <matiduck@gmail.com>
2016-05-07 16:34:12 +0200

    second

--- homework-26-04-2016 /app.js
+++ homework-26-04-2016 /app.js
@@ -2,7 +2,7 @@ var addItem = function(){
   var list = document.querySelector("#todo-list");
   var listItem = document.createElement("li");
 
-  var input = document.querySelector("#imput-text").value;
+  var input = document.querySelector("#textInput").value;
   var liText = document.createTextNode(input);
 
   listItem.appendChild(liText);
@@ -12,15 +12,11 @@ var addItem = function(){
 var checkboxCounter = function(){
   counter = document.querySelector("#boxCounter");
   counter.textContent =  document.querySelectorAll('input[type="checkbox"]:checked').length;
-
+}
 var refreshCounter = function(){
   var list = document.querySelector("#todo-list");
   var counter = document.querySelector("#counter");
   counter.textContent = list.querySelectorAll("li").length;
 }
 
-var addButton = document.querySelector("#input-button");
-addButton.addEventListener('click', function(){
-  addItem();
-  refreshCounter();
-});
+document.getElementById("buttonInput").addEventListener("click". alert("mhm"));
