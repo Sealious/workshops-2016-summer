5f0be4675a7e0c5d160c9a69c764784ce5084e36
matiduck <matiduck@gmail.com>
2016-05-07 19:49:30 +0200

    need to fix done nodes

--- homework-26-04-2016 /app.js
+++ homework-26-04-2016 /app.js
@@ -1,22 +1,40 @@
-var addItem = function(){
-  var list = document.querySelector("#todo-list");
-  var listItem = document.createElement("li");
+var addItem = function() {
+    var list = document.querySelector("#todo-list");
+    var listItem = document.createElement("li");
 
-  var input = document.querySelector("#textInput").value;
-  var liText = document.createTextNode(input);
+    var input = document.querySelector("#textInput").value;
+    var liText = document.createTextNode(input);
 
-  listItem.appendChild(liText);
-  list.appendChild(listItem);
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "id";
+
+    var label = document.createElement("label");
+    label.setAttribute("for", checkbox.id);
+    label.appendChild(liText);
+
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    list.appendChild(listItem);
+  }
+
+var refreshCounter = function() {
+    var list = document.querySelector("#todo-list");
+    var counter = document.querySelector("#counter");
+    counter.textContent = list.querySelectorAll("li").length;
 }
 
-var checkboxCounter = function(){
-  counter = document.querySelector("#boxCounter");
-  counter.textContent =  document.querySelectorAll('input[type="checkbox"]:checked').length;
-}
-var refreshCounter = function(){
-  var list = document.querySelector("#todo-list");
-  var counter = document.querySelector("#counter");
-  counter.textContent = list.querySelectorAll("li").length;
+var addButton = document.querySelector("#buttonInput");
+addButton.addEventListener("click", function() {
+    addItem();
+    refreshCounter();
+});
+
+var checkboxCounter = function() {
+    counter = document.querySelector("#boxCounter");
+    counter.textContent = document.querySelectorAll('input[type="checkbox"]:checked').length;
 }
 
-document.getElementById("buttonInput").addEventListener("click". alert("mhm"));
+var checkbox = document.querySelectorAll('input[type="checkbox"]');
+checkbox.addEventListener("click",checkboxCounter());
