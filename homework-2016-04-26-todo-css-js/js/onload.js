824bcdc029cecfb08455e2142620fe9e0f5f0087
Mateusz Teklak <mt667@wp.pl>
2016-05-10 18:21:21 +0200

    użycie 'submit' forma

--- homework-2016-04-26-todo-css-js/js/onload.js
+++ homework-2016-04-26-todo-css-js/js/onload.js
@@ -1,20 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var addButton = document.querySelector("#addButton");
-    addButton.addEventListener('click', function () {
+    var textInput = document.querySelector('input[type=text]');
+    textInput.addEventListener('keypress', function () {
+        this.classList.remove("emptyText");
+    });
+
+    var form = document.querySelector("#todo-form");
+    form.addEventListener('submit', function (e){
+        e.preventDefault();
         var textInput = document.querySelector('input[type=text]');
         if(textInput.value.length == 0) {
             textInput.classList.add("emptyText");
         }
         addItem();
-    });
-
-    var textInput = document.querySelector('input[type=text]');
-    textInput.addEventListener('keypress', function (e) {
-        var key = e.which || e.keyCode;
-        if (key === 13) { // 13 is enter
-            addItem();
-        }
-        this.classList.remove("emptyText");
     });
     render();
 });
