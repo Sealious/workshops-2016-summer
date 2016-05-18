d6a2a5cb661edc8f138c61bbe0b0d9b185e8db77
Mieszko Wrzeszczyński <mieszkobor@op.pl>
2016-05-18 10:39:34 +0200

    added checkboxes event

--- homework4/app.js
+++ homework4/app.js
@@ -1,13 +1,13 @@
 var tasks = [];
 
-
-function render_task(task_data){
+function render_task(task_data, index){
   var list = document.getElementById("to-do-list");
   var list_element = document.createElement("li");
   var item_title = document.createElement("div");
   var item_date = document.createElement("span");
   var item_checked = document.createElement("span");
   var icon = document.createElement("i");
+
   item_title.className = "title";
   item_date.className = "date";
   icon.className = "fa fa-clock-o";
@@ -20,43 +20,45 @@ function render_task(task_data){
 
   checkbox.type="checkbox";
 
-  if(task_data.done == true){
-  	checkbox.checked = "checked";
-  }
+  if(task_data.done == true)
+    checkbox.checked = "checked";
+
+  checkbox["data-task-id"] = index;
+  checkbox.onclick = checkbox_clicked;
 
   list_element.appendChild(item_title);
   list_element.appendChild(item_date);
   item_checked.appendChild(checkbox);
-
   item_date.appendChild(icon);
+
   item_date.innerHTML += " " + task_data.date_now;
 
   item_checked.appendChild(checked_info);
-
   list_element.appendChild(item_checked);
    
   list.appendChild(list_element);
   list.insertBefore(list_element, list.firstChild)
 }
 
-function render(){
-	var list = document.getElementById("to-do-list");
+function checkbox_clicked(event){
+  index = event.target["data-task-id"];
+  tasks[index].done = event.target.checked;
+}
 
+function render(){
+  var list = document.getElementById("to-do-list");
 
   while (list.hasChildNodes())
-  {
       list.removeChild(list.lastChild);
-  }
 
   tasks.forEach(render_task);
 }
 
 render();
 
-
 function addTask(){
-	var date_now = new Date().toJSON().slice(0,10);
-	var title =  document.getElementById('new-title').value;
+  var date_now = new Date().toJSON().slice(0,10);
+  var title =  document.getElementById('new-title').value;
 
   if(title == "")
   {
@@ -66,8 +68,8 @@ function addTask(){
   else
      document.getElementById('new-title').style.border = '2px solid #ccc'; 
 
-  var done = true;
-	tasks.push({title,date_now,done});
+  var done = false;
+  tasks.push({title,date_now,done});
   render();
   document.getElementById('new-title').value = "";
 }
