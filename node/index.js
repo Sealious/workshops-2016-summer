b2778248f11b28cb08d99f313580a7ab200b9a4d
Mateusz Teklak <mt667@wp.pl>
2016-05-31 18:41:49 +0200

    post

--- node/index.js
+++ node/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
-var cors = require('cors');
 var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var test = require('./app');
 test.asd('asd');
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    {extended: true}
+));
 
 app.get('/', function (req, res) {
     console.log('GET');
@@ -17,6 +22,10 @@ app.get('/json', function (req, res) {
     res.send({test: "value"});
 });
 
+app.post('/test', function (req, res) {
+    console.log("POST /test");
+    console.log(req.body);
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
