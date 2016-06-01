d07f928a40b368856759ff1d9ca8a909d8d9841a
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-06-01 21:14:28 +0200

    Add GET routes

--- own-api/index.js
+++ own-api/index.js
@@ -5,6 +5,16 @@ var logger = require('morgan');
 
 var app = express();
 
+var lastId = 1;
+
+var tasks = [
+  {
+    id: 1,
+    is_done: false,
+    title: "Some title",
+  }
+];
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +22,18 @@ app.use(logger('dev'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/tasks', function(req, res) {
+  res.json(tasks);
+});
+
+app.get('/task/:id', function(req,res) {
+  var task = tasks.filter(task => task.id == req.params.id)[0];
+  if(!task) {
+    res.sendStatus(404);
+  }
+  res.json(task);
 });
 
 app.listen(4750, function () {
