7a6e8da8a21981824d744f2b6995167cb93ebe85
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-06-01 20:54:47 +0200

    Add logger (morgan package)

--- own-api/index.js
+++ own-api/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 var app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
